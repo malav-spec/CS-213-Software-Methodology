008c51f3e56e3881a5aae1036d891126d789af37
Malav Doshi <56772095+malav-spec@users.noreply.github.com>
2021-01-26 20:37:38 +0530

    Update Kiosk.java

--- Project1/Kiosk.java
+++ Project1/Kiosk.java
@@ -7,11 +7,14 @@ public class Kiosk {
     public String name,command,number,dateIn;
     Date date;
 
+    int serialNumber = 10001;
 
     public void run(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Library Kiosk running.");
 
+
+        Library lib = new Library();
 
         for(;;){//Infinite loop to keep the session running
 
@@ -28,7 +31,11 @@ public class Kiosk {
                 command = st.nextToken();
             }
 
-            if(isLowerCase(command)){
+            if(!validCommand(command)){
+                System.out.println("Invalid command!");
+            }
+
+            if(isLowerCase(command)){//Check for lower case commands
                 System.out.println("Invalid command!");
                 continue;
             }
@@ -50,10 +57,59 @@ public class Kiosk {
                 date = new Date(dateIn);
 
                 if(!date.isValid()){//If date not valid give a warning
-                    System.out.println("Invalid Command!");
+                    System.out.println("Invalid Date!");
                     continue;
                 }
             }
+
+            if(command.equals("A")){ //Calls the Add command from the library
+                Book newBook = new Book();
+                newBook.setName(name);
+                newBook.setDatePublished(date);
+                newBook.setNumber(Integer.toString(serialNumber));
+                lib.add(newBook);
+                serialNumber++; //increase sr. number after adding
+                System.out.println(name + " added to the library.");
+                continue;
+            }
+            else if(command.equals("R")){ //Calls the remove function from the lib
+                Book newBook = new Book();
+                newBook.setNumber(number);
+                if(lib.remove(newBook)){
+                    System.out.println("Book#"+number+" removed.");
+                }
+                else{
+                    System.out.println("Unable to remove, the library does not have this book.");
+                }
+            }
+            else if(command.equals("O")){ //Calls the checkOut function from the lib
+                Book newBook = new Book();
+                newBook.setNumber(number);
+                if(!lib.checkOut(newBook)){
+                    System.out.println("Book#"+number+" is not available");
+                }
+                else{
+                    System.out.println("You've checked out "+"Book#"+number+". Enjoy!");
+                }
+
+            }
+            else if(command.equals("I")){ // Calls the returns function from the lib
+                Book newBook = new Book();
+                newBook.setNumber(number);
+                if(!lib.returns(newBook)){
+                    System.out.println("Unable to return "+"Book#"+number+".");
+                }
+                else{
+                    System.out.println("Book#"+number+" return has completed. Thanks!");
+                }
+            }
+
+            else if(command.equals("PA")){ //Calls the print function from the lib
+                System.out.println("** List of books in the library");
+                lib.print();
+                System.out.println("** End of list");
+            }
+
         }
     }
 
@@ -76,6 +132,13 @@ public class Kiosk {
             if(Character.isLowerCase(cmd.charAt(i))){
                 return true;
             }
+        }
+        return false;
+    }
+
+    public boolean validCommand(String cmd){
+        if(cmd.equals("A") || cmd.equals("R") || cmd.equals("I") || cmd.equals("O") || cmd.equals("PA") || cmd.equals("PD") || cmd.equals("PN")){
+            return true;
         }
         return false;
     }
